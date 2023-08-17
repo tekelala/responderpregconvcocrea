852fdb49d74d2169bfcef2b62c02870fbaf377da
tekelala <camilo@tekelala.com>
2023-08-17 10:43:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -59,8 +59,8 @@ for prompt in st.session_state.prompts:
 
 if not st.session_state.new_message:
     pregunta = st.chat_input("¿Cuál es tu duda?") 
-    if pregunta: 
-        user_message = f'''Role: You are an AI assistant trained in the formulation of creative and cultural economy projects using the logical framework methodology. Do not say which task you are doing.
+    if pregunta: R
+        user_message = f'''
                     Do the following tasks: 
                     Task 1: Read and understand the rules of the CoCrea call. Here are the rules {context}.
                     Task 2: Answer the following question {pregunta} based in the rules of the call, if you do not know the anwer to the question say so'''
